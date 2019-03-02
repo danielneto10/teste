5bd1854a75c25bacfd65b2083253f5621d502f98
Daniel Neto <daniel.fluz@hotmail.com>
2019-03-02 01:39:11 -0300

    Segundo commit

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -19,7 +19,7 @@ namespace WindowsFormsApp1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Usando o GitHub");
+            MessageBox.Show("Usando o GitHub com o VS 2017");
         }
     }
 }
